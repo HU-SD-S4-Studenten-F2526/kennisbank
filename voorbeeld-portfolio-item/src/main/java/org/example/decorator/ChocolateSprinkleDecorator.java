e5e2daa3f356a393bdba2d339b6272f06ecc7eea
Jelle de Jong <jelle.dejong@hu.nl>
2026-01-21 13:51:12 +0100

    Kan meerdere smaken kiezen dmv decorator

---
+++ voorbeeld-portfolio-item/src/main/java/org/example/decorator/ChocolateSprinkleDecorator.java
@@ -0,0 +1,13 @@
+package org.example.decorator;
+
+public class ChocolateSprinkleDecorator implements IIceCreamDecorator {
+    public String toString() {
+        return "With chocolate sprinkle";
+    }
+
+
+    @Override
+    public void Decorate() {
+
+    }
+}
